22b8437b4e979a3819b8edb929601acc6e80da06
Sebastian Castro <sebas.a.castro@gmail.com>
2025-01-11 17:55:15 -0500

    Rebase from 'release/humble/py_trees'

--- docs/examples/skeleton_tree.py
+++ docs/examples/skeleton_tree.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""Example showing how to create and tick a simple behaviour tree."""
 
 import py_trees
 
-if __name__ == '__main__':
-
-    root = py_trees.composites.Selector("Selector")
+if __name__ == "__main__":
+    root = py_trees.composites.Selector("Selector", memory=False)
     high = py_trees.behaviours.Success(name="High Priority")
     med = py_trees.behaviours.Success(name="Med Priority")
     low = py_trees.behaviours.Success(name="Low Priority")
     root.add_children([high, med, low])
 
-    behaviour_tree = py_trees.trees.BehaviourTree(
-        root=root
-    )
+    behaviour_tree = py_trees.trees.BehaviourTree(root=root)
     print(py_trees.display.unicode_tree(root=root))
     behaviour_tree.setup(timeout=15)
 
-    def print_tree(tree):
+    def print_tree(tree: py_trees.trees.BehaviourTree) -> None:
+        """Print the behaviour tree and its current status."""
         print(py_trees.display.unicode_tree(root=tree.root, show_status=True))
 
     try:
@@ -25,7 +24,7 @@ if __name__ == '__main__':
             period_ms=500,
             number_of_iterations=py_trees.trees.CONTINUOUS_TICK_TOCK,
             pre_tick_handler=None,
-            post_tick_handler=print_tree
+            post_tick_handler=print_tree,
         )
     except KeyboardInterrupt:
         behaviour_tree.interrupt()
