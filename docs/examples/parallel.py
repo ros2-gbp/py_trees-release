91ee238b0617cbaf0c02f1224d4c3a4a39f971be
Sebastian Castro <sebas.a.castro@gmail.com>
2025-01-11 17:57:21 -0500

    Rebase from 'debian/jazzy/py_trees'

--- docs/examples/parallel.py
+++ docs/examples/parallel.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""Example demonstrating the use of a parallel control node."""
 
 import py_trees
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     b1 = py_trees.behaviours.Success(name="B1")
     b2 = py_trees.behaviours.Success(name="B2")
     b3 = py_trees.behaviours.Success(name="B3")
     root = py_trees.composites.Parallel(
+        name="Parallel",
         policy=py_trees.common.ParallelPolicy.SuccessOnSelected(
-            synchronise=True,
-            children=[b1, b2]
-        )
+            synchronise=True, children=[b1, b2]
+        ),
     )
     root.add_children([b1, b2, b3])
-    py_trees.display.render_dot_tree(root, py_trees.common.string_to_visibility_level("all"))
+    py_trees.display.render_dot_tree(
+        root, py_trees.common.string_to_visibility_level("all")
+    )
