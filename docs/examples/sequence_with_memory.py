c23668c1eae2c768ca9fa62fe2610522442b7c95
Sebastian Castro <sebas.a.castro@gmail.com>
2025-01-11 17:59:16 -0500

    Rebase from 'debian/rolling/py_trees'

--- docs/examples/sequence_with_memory.py
+++ docs/examples/sequence_with_memory.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""Example demonstrating the use of a sequence control node with memory."""
 
 import py_trees
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     root = py_trees.composites.Sequence(name="Sequence with Memory", memory=True)
     guard = py_trees.behaviours.Success(name="Guard")
     a1 = py_trees.behaviours.Success(name="Action 1")
     a2 = py_trees.behaviours.Success(name="Action 2")
     a3 = py_trees.behaviours.Success(name="Action 3")
     root.add_children([guard, a1, a2, a3])
-    py_trees.display.render_dot_tree(root, py_trees.common.string_to_visibility_level("all"))
+    py_trees.display.render_dot_tree(
+        root, py_trees.common.string_to_visibility_level("all")
+    )
